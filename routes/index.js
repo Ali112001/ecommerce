2fa5b624fefb045785a5331231a522385dfbc3f9
unknown <doaahadyhady@gmail.com>
2023-04-16 17:00:48 +0200

    session 19

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const reviewRoute = require("./reviewRoute");
 const wishlistRoute = require("./wishlistRoute");
 const addressRoute = require("./addressRoute");
 const couponRoute = require("./couponRoute");
-// const cartRoute = require("./cartRoute");
+const cartRoute = require("./cartRoute");
 
 const mountRoutes = (app) => {
   app.use("/api/v1/categories", categoryRoute);
@@ -21,7 +21,7 @@ const mountRoutes = (app) => {
   app.use("/api/v1/wishlist", wishlistRoute);
   app.use("/api/v1/addresses", addressRoute);
   app.use("/api/v1/coupons", couponRoute);
-  //   app.use("/api/v1/cart", cartRoute);
+  app.use("/api/v1/cart", cartRoute);
 };
 
 module.exports = mountRoutes;
