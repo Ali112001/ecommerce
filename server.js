82c4e9b33c6a4096e99172bf369672008789f136
unknown <doaahadyhady@gmail.com>
2023-04-27 23:14:28 +0200

    session 21

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const cors = require("cors");
+const compression = require("compression");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
 
 dotenv.config({ path: "config.env" });
 const ApiError = require("./utils/apiError");
@@ -10,6 +14,7 @@ const globalError = require("./middlewares/errorMiddleware");
 const dbConnection = require("./config/database");
 // Routes
 const mountRoutes = require("./routes");
+const { webhookCheckout } = require("./services/orderService");
 
 // Connect with db
 dbConnection();
@@ -17,14 +22,52 @@ dbConnection();
 // express app
 const app = express();
 
+// Enable other domains to access your application
+app.use(cors());
+app.options("*", cors());
+
+// Compress all responses
+app.use(compression());
+
+// Checkout webhook
+app.post(
+  "/webhook-checkout",
+  express.raw({ type: "application/json" }),
+  webhookCheckout
+);
+
 // Middlewares
-app.use(express.json());
+app.use(express.json({ limit: "20kb" }));
 app.use(express.static(path.join(__dirname, "uploads")));
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
   console.log(`mode: ${process.env.NODE_ENV}`);
 }
+
+// Limit each IP to 100 requests per `window` (here, per 15 minutes)
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+  message:
+    "Too many accounts created from this IP, please try again after 15 min",
+});
+
+// Apply the rate limiting middleware to all requests
+app.use("/api", limiter);
+
+// Middleware to protect against HTTP Parameter Pollution attacks
+app.use(
+  hpp({
+    whitelist: [
+      "price",
+      "sold",
+      "quantity",
+      "ratingsQuantity",
+      "ratingsAverage",
+    ],
+  })
+);
 
 // Mount Routes
 mountRoutes(app);
